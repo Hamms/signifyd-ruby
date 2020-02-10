19a75810bf40376d6564912689c42162f5948191
Y. Wayne Huang <wayne530@gmail.com>
2020-02-10 12:57:35 -0800

    Convert order_id to string

--- lib/signifyd/api/fulfillment/create.rb
+++ lib/signifyd/api/fulfillment/create.rb
@@ -5,7 +5,7 @@ module Signifyd
         module ClassMethods
           def create(order_id, params={}, api_key=nil)
             raise InvalidRequestError.new('You have passed invalid parameters to Fulfillment.create') if params == {}
-            Signifyd.request(:post, self.url + '/' + CGI.escape(order_id), params, api_key)
+            Signifyd.request(:post, self.url + '/' + CGI.escape(order_id.to_s), params, api_key)
           end
         end
 
